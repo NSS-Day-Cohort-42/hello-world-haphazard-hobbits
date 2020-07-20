cef396abf1b2659e1c7a9fd8f03027ffe896c15f
mgjeffries <gib.jeffries@gmail.com>
2020-07-20 11:40:16 -0500

    add data providers

---
+++ scripts/iceland/Landmarks/LandmarksDataProvider.js
@@ -0,0 +1,20 @@
+const landmarks = [
+  {
+    name: "Thingvellir National Park - Rugged volcanic national park & lakes",
+    link: "https://www.thingvellir.is/en/"
+  },
+  {
+    name: "Gullfoss Falls - Famed, multi-section waterfall",
+    link: "https://en.wikipedia.org/wiki/Gullfoss"
+  },
+  {
+    name: "Golden Circle - Aurora, geyser, glacier, and hot spring",
+    link: "https://www.nordicvisitor.com/blog/driving-golden-circle-how-to-guide/"
+  },
+  {
+    name: "Jökulsárlón - Big glacial lake with icebergs & seals",
+    link: "https://en.wikipedia.org/wiki/J%C3%B6kuls%C3%A1rl%C3%B3n"
+  },
+]
+
+export const useLandmarks = () => landmarks.slice()
