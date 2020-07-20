d36e91a11f206d1a074f940e2a353c6d949e8e2d
mgjeffries <gib.jeffries@gmail.com>
2020-07-20 11:55:36 -0500

    fix heads class

--- scripts/iceland/HeadsOfState/HeadsOfStateHTML.js
+++ scripts/iceland/HeadsOfState/HeadsOfStateHTML.js
@@ -1,6 +1,6 @@
 export const headsOfStateHTML = (headOfState) => {
   return `
-  <li class="city__item">
+  <li class="heads__item">
     <a href="${headOfState.link}"><strong>${headOfState.title} </strong>${headOfState.name}</a>
   </li>
   `
