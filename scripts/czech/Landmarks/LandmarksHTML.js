1443126f68554ec8277b4c8dd4a8d22ec4fa5033
Connor Blakeney <connorblakeney@yahoo.com>
2020-07-20 11:34:49 -0500

    automated landmarks

---
+++ scripts/czech/Landmarks/LandmarksHTML.js
@@ -0,0 +1,7 @@
+export const landmarksHTML = (landmark) => {
+  return `
+      <li class="landmark__item">
+        <a href="${landmark.link}">${landmark.name}</a>
+      </li>
+      `
+}
--- scripts/czech/main.js
+++ scripts/czech/main.js
@@ -1,3 +1,5 @@
 import { listCities } from "./Cities/CitiesList.js"
+import { listLandmarks } from "./Landmarks/LandmarksList.js"
 
 listCities()
+listLandmarks()
