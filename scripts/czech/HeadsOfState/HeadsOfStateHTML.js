5a114dbd988e9698aa7105b3dd67de79127243f5
Connor Blakeney <connorblakeney@yahoo.com>
2020-07-20 13:41:46 -0500

    automated heads of state

---
+++ scripts/czech/HeadsOfState/HeadsOfStateHTML.js
@@ -0,0 +1,7 @@
+export const headsOfStateHTML = (headOfState) => {
+  return `
+    <li class="heads__item">
+      <a href="${headOfState.link}"><strong>${headOfState.title} </strong>${headOfState.name}</a>
+    </li>
+    `
+}
--- scripts/czech/main.js
+++ scripts/czech/main.js
@@ -1,5 +1,7 @@
 import { listCities } from "./Cities/CitiesList.js"
 import { listLandmarks } from "./Landmarks/LandmarksList.js"
+import { listHeadsOfState } from "./HeadsOfState/HeadsOfStateList.js"
 
 listCities()
 listLandmarks()
+listHeadsOfState()
