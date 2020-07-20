e3aed27aa246abbb99771d05a704c6371e09986e
heymonicakay <hey.monicakay@gmail.com>
2020-07-20 14:24:40 -0500

    edits

--- scripts/the-netherlands/main.js
+++ scripts/the-netherlands/main.js
@@ -1,3 +1,7 @@
 import { listCities } from "./Cities/CitiesList.js"
+import { listLandmarks } from "./Landmarks/LandmarksList.js";
+import { listHeads } from "./HeadsOfState/HeadsOfStateList.js";
 
 listCities()
+listLandmarks()
+listHeads()
