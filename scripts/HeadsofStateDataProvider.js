95ff0f178da9b43c0ec69abee017ba87f3903a8e
Connor Blakeney <connorblakeney@yahoo.com>
2020-07-21 09:17:38 -0500

    first commit, moved heads of state data to one file

---
+++ scripts/HeadsofStateDataProvider.js
@@ -0,0 +1,93 @@
+const headsofState = [
+  [
+    {
+      title: "Prime Minister",
+      name: "Mark Rutte",
+      link:
+        "https://en.wikipedia.org/wiki/Prime_Minister_of_the_Netherlands#:~:text=The%20Prime%20Minister%20is%20de,Rutte%2C%20in%20office%20since%202010.",
+    },
+    {
+      title: "First Deputy Prime Minister",
+      name: "Hugo de Jonge",
+      link: "https://en.wikipedia.org/wiki/Hugo_de_Jonge",
+    },
+    {
+      title: "Second Deputy Prime Minister",
+      name: "Kajsa Ollongren",
+      link: "https://en.wikipedia.org/wiki/Kajsa_Ollongren",
+    },
+    {
+      title: "Third Deputy Prime Minister",
+      name: "Carola Schouten",
+      link: "https://en.wikipedia.org/wiki/Carola_Schouten",
+    },
+  ],
+  [
+    {
+      title: "President",
+      name: "Michael D Higgins",
+      link: "https://president.ie/en/the-president/michael-d-higgins",
+    },
+    {
+      title: "Prime Minister",
+      name: "Michael Martin",
+      link:
+        "https://www.nytimes.com/2020/06/27/world/europe/ireland-prime-minister-micheal-martin.html",
+    },
+    {
+      title: "Leader of the Opposition",
+      name: "Mary lou McDonald",
+      link: "https://en.wikipedia.org/wiki/Mary_Lou_McDonald",
+    },
+    {
+      title: "Tanaiste",
+      name: "Leo Eric Varadkar",
+      link:
+        "https://www.finegael.ie/our-people/ministers/dublin/dublin-west/leo-varadkar/",
+    },
+  ],
+  [
+    {
+      name: "Guðni Th. Jóhannesson",
+      link: "https://en.wikipedia.org/wiki/Gu%C3%B0ni_Th._J%C3%B3hannesson",
+      title: "President:",
+    },
+    {
+      name: "parliment with 63 members",
+      link: "https://en.wikipedia.org/wiki/Althing",
+      title: "The Althing",
+    },
+    {
+      name: "Þorgeir Örlygsson",
+      link: "https://www.bloomsbury.com/author/thorgeir-orlygsson",
+      title: "Supreme Court President",
+    },
+  ],
+  [
+    {
+      name: "Miloš Zeman",
+      link: "https://en.wikipedia.org/wiki/Milo%C5%A1_Zeman",
+      title: "President: ",
+    },
+    {
+      name: "Miloš Vystrčil",
+      link: "https://en.wikipedia.org/wiki/Milo%C5%A1_Vystr%C4%8Dil",
+      title: "President of the Senate: ",
+    },
+    {
+      name: "Andrej Babiš",
+      link: "https://en.wikipedia.org/wiki/Andrej_Babi%C5%A1",
+      title: "Prime Minister: ",
+    },
+    {
+      name: "Radek Vondráček",
+      link: "https://en.wikipedia.org/wiki/Radek_Vondr%C3%A1%C4%8Dek",
+      title: "Speaker of the Chamber of Deputies: ",
+    },
+  ],
+]
+
+export const netherlandsHeadsOfState = () => headsofState.slice(0, 1)
+export const irelandHeadsOfState = () => headsofState.slice(1, 2)
+export const icelandHeadsOfState = () => headsofState.slice(2, 3)
+export const czechHeadsOfState = () => headsofState.slice(3, 4)
