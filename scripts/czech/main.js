45b8d93e4f40c139fd4b115809aa920080fd385d
mgjeffries <gib.jeffries@gmail.com>
2020-07-21 10:07:16 -0500

    remove city specific folders

--- scripts/czech/main.js
+++
@@ -1,5 +0,0 @@
-import { listCities } from "./Cities/CitiesList.js"
-import { listLandmarks } from "./Landmarks/LandmarksList.js"
-
-listCities()
-listLandmarks()
--- scripts/the-netherlands/HeadsOfState/HeadsOfStateHTML.js
+++
@@ -1,7 +0,0 @@
-export const headsHTML = (head) => {
-      return `
-        <li class="heads__item">
-          <a href="${head.link}">${head.name}</a>
-        </li>
-        `
-    }
